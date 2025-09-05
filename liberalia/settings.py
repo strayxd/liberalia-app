28a8ef1b62f0647ffcb78ee134175b2781a6c499
Grupo2Duoc <riv.uribe@duocuc.cl>
2025-09-05 07:09:10 -0400

    Update

--- liberalia/settings.py
+++ liberalia/settings.py
@@ -77,6 +77,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -91,11 +92,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
